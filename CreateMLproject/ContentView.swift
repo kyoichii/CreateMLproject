59a2a1a53ba201284c667eceb130f474169cbe2b
Kyoichi <kyoichi.807@gmail.com>
2022-06-09 12:06:01 +0900

    first commit

---
+++ CreateMLproject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreateMLproject
+//
+//  Created by 田野恭一 on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreateMLproject/CreateMLprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateMLprojectApp.swift
+//  CreateMLproject
+//
+//  Created by 田野恭一 on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct CreateMLprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
